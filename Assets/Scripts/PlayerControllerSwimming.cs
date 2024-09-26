f0527a8c4de1b12be6a4fb96f6bf3e975dc558b5
Tre <66348444+TreWestWilliam@users.noreply.github.com>
2024-09-26 03:26:44 -0400

    Improved Character Controlling
    Character only swims forward now, and rotates based on camera direction plus movement direction

--- Assets/Scripts/PlayerControllerSwimming.cs
+++ Assets/Scripts/PlayerControllerSwimming.cs
@@ -1,3 +1,4 @@
+/// Player Controller Swimming by Hachiski
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
@@ -9,14 +10,18 @@ public class PlayerControllerSwimming : MonoBehaviour
     public Rigidbody _Rigidbody;
     public float Movespeed = 25;
     public float VerticalMovespeed = 10;
+    public float RotationalSpeed = 5;
+    public float SwimBoostSpeed = 20;
 
     [Header("Camera Controls")]
     public Camera PlayerCamera;
     public float CameraDistance = 10;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        // Lock the cursor on start for mouse+keyboard input
         Cursor.lockState= CursorLockMode.Locked;
         Cursor.visible = false;
         
@@ -27,11 +32,14 @@ public class PlayerControllerSwimming : MonoBehaviour
     {
 
         Vector2 MovementInput = _PlayerInput.actions["Move"].ReadValue<Vector2>();
-        MovementInput *= Time.deltaTime;
-        MovementInput *= Movespeed;
-        _Rigidbody.AddForce(transform.forward * MovementInput.y + transform.right * MovementInput.x);
-        _Rigidbody.MoveRotation( Quaternion.Lerp(transform.rotation,Quaternion.Euler(0,PlayerCamera.transform.rotation.eulerAngles.y,0 ), .2f ));
+        MovementInput *= Time.deltaTime * Movespeed;
+        
+        _Rigidbody.AddForce(transform.forward * MovementInput.magnitude);
+        // Our rotation is basically the Camera's Rotation + The Input Direction Linerally Interpolated from our previous rotation
+        _Rigidbody.MoveRotation( Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(0,(PlayerCamera.transform.rotation.eulerAngles.y - Vector2.SignedAngle(Vector2.up, MovementInput)),0)  ), RotationalSpeed  * MovementInput.magnitude));
 
+
+        //Basic orbiting camera
         Vector2 CameraInput = _PlayerInput.actions["Look"].ReadValue<Vector2>();
         PlayerCamera.transform.position += PlayerCamera.transform.right * CameraInput.x * Time.deltaTime + PlayerCamera.transform.up * CameraInput.y * Time.deltaTime;
         PlayerCamera.transform.LookAt(transform);
@@ -39,32 +47,25 @@ public class PlayerControllerSwimming : MonoBehaviour
 
 
     }
-
+    // We may wish to adjust the forward momentum gain in the future since it's quite significant
     public void SwimUp(CallbackContext callbackContext) 
     {
         //TODO COOLDOWNS? (Probably through coroutines or something)
-        _Rigidbody.AddForce(transform.up *VerticalMovespeed + (transform.forward * Movespeed));
+        _Rigidbody.AddForce(transform.up *VerticalMovespeed + (transform.forward * SwimBoostSpeed));
     }
 
     public void SwimDown(CallbackContext callback) 
     {
-        _Rigidbody.AddForce(-transform.up *VerticalMovespeed + (transform.forward * Movespeed));
+        _Rigidbody.AddForce(-transform.up *VerticalMovespeed + (transform.forward * SwimBoostSpeed));
     }
 
+
+    //These functions were from testing, didn't work out since it only moved whenver the controller changed directions
     public void Move(CallbackContext callbackContext) 
     {
-        //Vector2 Input = callbackContext.ReadValue<Vector2>();
-        //Debug.Log($"MOVE INPUT SANITY TEST!  X: {Input.x}, Y: {Input.y}");
     }
 
     public void MoveCamera( CallbackContext callbackContext) 
-    {/*
-        Vector2 Input = callbackContext.ReadValue<Vector2>();
-        //Debug.Log($"CAMERA INPUT SANITY TEST!  X: {Input.x}, Y: {Input.y}");
-
-        PlayerCamera.transform.position += PlayerCamera.transform.right * Input.x *Time.deltaTime + PlayerCamera.transform.up * Input.y *Time.deltaTime;
-        PlayerCamera.transform.LookAt(transform);
-        PlayerCamera.transform.position = transform.position - PlayerCamera.transform.forward * CameraDistance;
-        */
+    {
     }
 }
